d594a72065a953917ec0a54c80fb73edc0aca83d
sbx_user1051 <sbx_user1051@169.254.3.89>
2025-12-17 10:49:50 +0000

    Wed Dec 17 10:49:50 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,33 @@
+from dataclasses import dataclass
+from typing import Optional
+from decimal import Decimal
+
+@dataclass
+class Offer:
+    Quantity : int
+    Price : Decimal
+
+@dataclass
+class Prices:
+    Item : str
+    Price : Decimal
+    SpecialOffer : Optional[Offer] = None
+
 
 class CheckoutSolution:
 
+    def __init__(self):       
+        self.prices = {
+            'A': Prices(Item='A', Price=Decimal('50'), SpecialOffer=Offer(Quantity=3, Price=Decimal('130'))),
+            'B': Prices(Item='B', Price=Decimal('30'), SpecialOffer=Offer(Quantity=2, Price=Decimal('45'))),
+            'C': Prices(Item='C', Price=Decimal('20')),
+            'D': Prices(Item='D', Price=Decimal('15')),
+        }
+
+        
+
     # skus = unicode string
     def checkout(self, skus):
+
         raise NotImplementedError()
+
